95fa8f1bda8f5aaae6963623b5b834e2fe861be7
arianaaghajani <aghajaniariana@gmail.com>
2024-02-23 15:26:34 +0330

    test BiPredicate

--- src/main/java/MyPredicate.java
+++ src/main/java/MyPredicate.java
@@ -1,14 +1,21 @@
+import java.time.LocalDate;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class MyPredicate {
     public static void main(String[] args) {
-        Predicate<String> isEmpty = a -> a.isEmpty();
-        System.out.println(isEmpty.test(""));
-        System.out.println(isEmpty.test("salam"));
+//        Predicate<String> isEmpty = a -> a.isEmpty();
+//        System.out.println(isEmpty.test(""));
+//        System.out.println(isEmpty.test("salam"));
+//
+//        Predicate<String> isAFirst = s -> Character.toUpperCase(s.charAt(0)) == 'A';
+//        System.out.println(isAFirst.test("aghajani"));
+//        System.out.println(isAFirst.test("jamshidi"));
+//        Predicate<LocalDate> moreThan18 = d -> (LocalDate.now().getYear() - d.getYear())>18;
+//        System.out.println(moreThan18.test(LocalDate.parse("1900-11-27")));
 
-        Predicate<String> isAFirst = s -> Character.toUpperCase(s.charAt(0)) == 'A';
-        System.out.println(isAFirst.test("aghajani"));
-        System.out.println(isAFirst.test("jamshidi"));
+        BiPredicate<String,Integer> numEqualStr = (a,b) -> Integer.parseInt(a) == b;
+        System.out.println(numEqualStr.test("11", 10));
     }
 }
 
